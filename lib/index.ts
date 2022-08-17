0bd9d9dd474e2ea0faccf840a4cb4f72d33a0ec9
facuariasla <facundolautaroarias@hotmail.com>
2022-08-17 17:41:30 -0300

    200 pokemons

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 export async function loadPokemons() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=250");
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=200");
   const data = await res.json();
   return data;
 }
