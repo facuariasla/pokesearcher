940b332dd8f715ca2263a47e9550b4c3f4fe8ce3
facuariasla <facundolautaroarias@hotmail.com>
2022-08-18 13:29:14 -0300

    cons deleted

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,6 @@ const Home: NextPage = ({ allPokeWImg }: any) => {
         el.name.includes(e.target.value.toLowerCase())
       );
       setArrayFragment(array);
-      console.log(array)
       setWord(true);
     } else {
       setWord(false);
