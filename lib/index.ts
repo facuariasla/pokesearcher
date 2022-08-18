26acc3df66d9052cacd4b1003fa7c59b39a369f2
facuariasla <facundolautaroarias@hotmail.com>
2022-08-18 11:01:54 -0300

    only call 100 pokemons in ssr - but all when the page is loaded - better performance

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
-export async function loadPokemons() {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=800");
+export async function loadPokemons(limit:number) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`);
   const data = await res.json();
   return data;
 }
